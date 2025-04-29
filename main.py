b2bc8241ae3e200432add01dd26986dc4a26e3eb
rangaroo <aslan.onalbek@nu.edu.kz>
2025-04-30 00:03:32 +0500

    added pygame groups

--- main.py
+++ main.py
@@ -6,25 +6,37 @@ from constants import *
 from player import *
 
 def main():
+
+    # Initializing the pygame and basic variables
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+
+    # Creating pygame groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill("black")
+        updatable.update(dt)
 
-        player.draw(screen)
-        player.update(dt)
+        screen.fill("black")
         
+        for drawable_object in drawable:
+            drawable_object.draw(screen)
+
         pygame.display.flip()
-        
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
