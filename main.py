4c43e5453582f21391fcfd3d0f92d629f039bbd5
rangaroo <aslan.onalbek@nu.edu.kz>
2025-04-29 23:48:56 +0500

    adding moving around

--- main.py
+++ main.py
@@ -19,7 +19,10 @@ def main():
                 return
         
         screen.fill("black")
+
         player.draw(screen)
+        player.update(dt)
+        
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
