1b869ea1516a322144de6a01df27a2bafbcb822b
rangaroo <aslan.onalbek@nu.edu.kz>
2025-05-02 01:41:45 +0500

    implemented asteroids' splitting behavior upon hit

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from circleshape import *
 from constants import *
 
@@ -8,6 +9,21 @@ class Asteroid(CircleShape):
     
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        random_angle = random.uniform(20, 50)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid_one = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_two = Asteroid(self.position.x, self.position.y, new_radius)
+
+        asteroid_one.velocity = self.velocity.rotate(random_angle) * 1.2
+        asteroid_two.velocity = self.velocity.rotate(-random_angle) * 1.2
 
     def update(self, dt):
         self.position += self.velocity * dt
